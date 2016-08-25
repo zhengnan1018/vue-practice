d3c6c0799bb197bf48e66820164efeb298eb0367
jessy.zheng@pwc.com <jessy.zheng@pwc.com>
2016-08-25 15:37:20 +0800

    build vue project structure

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import app from './app.vue'
+import VueRouter from 'vue-router'
+
+import './styles.css'
+
+Vue.config.debug = process.env.NODE_ENV !== 'production'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter()
+const App = Vue.extend(app)
+
+router.start(App, 'body')
